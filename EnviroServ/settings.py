a8205c2c819763b6d455e84a5473d9fc18bb475e
Alexander <74402359+alexanderwalford-official@users.noreply.github.com>
2021-02-05 16:24:55 +0000

    Added Error Handling Pages
    404 & 503 error codes are now handled.

--- EnviroServ/settings.py
+++ EnviroServ/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#lsp&cxrees6qo1m(&j474-yb+talps8um=klqw7hth7n!++g='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
