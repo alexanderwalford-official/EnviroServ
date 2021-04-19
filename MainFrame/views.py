8b62cf92c90d97344f0698129e1a9caee89fb7ee
Alexander <74402359+alexanderwalford-official@users.noreply.github.com>
2021-04-19 12:30:43 +0100

    Export As CSV
    Added an option to export the data collected into the .CSV format.

--- MainFrame/views.py
+++ MainFrame/views.py
@@ -11,6 +11,30 @@ from .models import Diagnostic_Issue
 from .models import AccountActivity
 
 
+def download_csv(modeladmin, request, queryset):
+    if not request.user.is_staff:
+        raise PermissionDenied
+    opts = queryset.model._meta
+    model = queryset.model
+    response = HttpResponse(mimetype='text/csv')
+    # force download.
+    response['Content-Disposition'] = 'attachment;filename=export.csv'
+    # the csv writer
+    writer = csv.writer(response)
+    field_names = [field.name for field in opts.fields]
+    # Write a first row with header information
+    writer.writerow(field_names)
+    # Write data rows
+    for obj in queryset:
+        writer.writerow([getattr(obj, field) for field in field_names])
+    return response
+download_csv.short_description = "Download selected as csv"
+
+@login_required 
+def exportSCV(request):
+    data = download_csv(ModelAdmin, request, SensorData.objects.order_by('-datetime'))
+    return HttpResponse (data, content_type='text/csv')
+
 @receiver(user_logged_in)
 def on_login(sender, user, request, **kwargs):
     AccountActivity_instance = AccountActivity.objects.create(activity="User Signed In",user = request.user)  
